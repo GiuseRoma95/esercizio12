ab0971456829bd5169c1269c5fa794ee62bff6b0
GiuseRoma95 <112175641+GiuseRoma95@users.noreply.github.com>
2022-09-13 16:37:55 +0200

    esercizio

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let varia1 = 1;
+console.log(varia1)
+
+var varia2 = 2;
+console.log(varia2)
+
+const varia3 = 3;
+console.log(varia3)
+
+//Differenza tra let e const
+
+//Let cambia valore
+
+varia1 = 6;
+console.log(varia1)
+
+//Const non permette il cambio
+
+// varia3 = 10 (script.js:19 Uncaught TypeError: Assignment to constant variable. at script.js:19:8) 
+//console.log(varia3)
+//--------------------------------------------------------------------------------------------------
+//Operazioni di somma e sottrazione
+let variatotale = varia1 + varia2 - varia3  //6+2-3=5
+console.log(variatotale)
+
+//Booleani
+
+let booleani = true; //NON MI È CHIARO L'UTILIZZO
+console.log(booleani);
